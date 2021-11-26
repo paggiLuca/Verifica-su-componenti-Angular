368f8e94828bb333c45abddafa33981c007ef46f
paggiLuca <paggi.luca@iisgalvanimi.edu.it>
2021-11-26 12:29:00 +0000

    punto 1-2

---
+++ angular-Paggi-Luca-fila-A/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent,
+    FourthComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
